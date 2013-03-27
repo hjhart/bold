a8a3bf3dba2882cbcd04b44720593b0ef56cf398
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-03-26 18:50:40 -0700

    Init gem

---
+++ spec/bold_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Bold do
+  it 'should have a version number' do
+    Bold::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end
