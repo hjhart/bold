fc894c6d05f7e95bbc76cdf98cfedd12da8125d1
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-03-26 19:18:46 -0700

    Bold can need and supply

--- lib/bold.rb
+++ lib/bold.rb
@@ -1,5 +1,26 @@
 require "bold/version"
 
 module Bold
-  # Your code goes here...
+  @suppliers = {}
+
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
+
+  def self.supplies(dep, &supplier)
+    @suppliers[dep] = supplier
+  end
+
+  def self.supply(dep)
+    @suppliers[dep].call
+  end
+
+  module ClassMethods
+    def needs(dep)
+      define_method(dep) do
+        @deps ||= {}
+        @deps[dep] ||= Bold.supply(dep)
+      end
+    end
+  end
 end
--- spec/bold_spec.rb
+++ spec/bold_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe Bold do
-  it 'should have a version number' do
-    Bold::VERSION.should_not be_nil
+  class Piano; end
+
+  Bold.supplies(:piano) { Piano.new }
+
+  class Bob
+    include Bold
+
+    needs :piano
   end
+
+  subject do
+    Bob.new
+  end
+
+  its(:piano) { should be_a(Piano) }
 end
